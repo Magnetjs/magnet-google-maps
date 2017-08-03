7469088850b446d12a7b611aac2bad0653a99f0c
Kieve Chua <kievechua@gmail.com>
2017-08-03 15:18:14 +0800

    update core

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import { Module } from 'magnet-core/module'
 import * as googleMaps from '@google/maps'
 
 export default class MagnetGoogleMaps extends Module {
-  get moduleName () { return '_google_maps' }
-  get defaultConfig () { return __dirname }
+  init () {
+    this.moduleName = '_google_maps'
+    this.defaultConfig = __dirname
+  }
 
   async setup () {
     this.insert(googleMaps.createClient(this.config))
